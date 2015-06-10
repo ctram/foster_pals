4594ca4f79e08c970d5fd38c4a0768c8a4ebf411
chris <chris@chriss-MacBook-Pro.local>
2015-06-10 10:42:48 -0700

    Restructured from fosterers and organizations to one single mode, the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get '/', to: 'static_pages#front'
-  get '/register', to: 'static_pages#register', as: :register
-  get '/sign-in', to: 'session#new'
+  get '/register', to: 'users#new', as: :register
+  get '/sign-in', to: 'sessions#new'
   get '/home', to: 'static_pages#home', as: :home
   post '/process_registration', to: 'static_pages#process_registration', as: :process_registration
 
   get '/root', to: 'static_pages#root'
-  get '/choose_role_sign_in', to: 'static_pages#choose_role_sign_in', as: :choose_role_sign_in
-  get '/choose_role_registration', to: 'static_pages#choose_role_registration', as: :choose_role_registration
 
   namespace :api do
+    resources :users, only: [:show, :edit, :update ]
+
     resources :fosterers, only: [:show, :edit, :update ]
     resources :organizations, only: [:show, :edit, :update ]
     resources :animals, only: [:show, :edit, :update ]
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
   end
 
   resource :session, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create]
+
   resources :fosterers, only: [:new, :create]
   resources :organizations, only: [:new, :create]
 
