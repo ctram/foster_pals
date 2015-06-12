7603db23c5fc10c410fe4299570b09bb6de41461
chris <chris@chriss-mbp.attlocal.net>
2015-06-11 23:06:23 -0700

    Add "indefinite stay" button.

--- app/assets/javascripts/views/users/scheduler/dates_picker/dates_picker.js
+++ app/assets/javascripts/views/users/scheduler/dates_picker/dates_picker.js
@@ -4,7 +4,17 @@ FosterPals.Views.DatesPicker = Backbone.CompositeView.extend({
   className: 'dates-picker col-md-8',
 
   events: {
-    'click button#check-aval-btn' : 'checkDates'
+    'click button#check-aval-btn' : 'checkDates',
+    'click input#indefinite-stay-checkbox' : 'lockCheckOutInput'
+  },
+
+  initialize: function (options) {
+    this.user = options.model;
+    this.checkOutInputToggled = false;
+
+
+
+
   },
 
   checkDates: function (event) {
@@ -15,15 +25,25 @@ FosterPals.Views.DatesPicker = Backbone.CompositeView.extend({
 
     var checkInDate = $checkIn.data("DateTimePicker").date()._d.toLocaleString();
     var checkOutDate = $checkOut.data("DateTimePicker").date()._d.toLocaleString();
+    if ($('#indefinite-stay-checkbox:checked').length === 1) {
+      var indefiniteStay = true;
+    } else {
+      var indefiniteStay = false;
+    }
+
+    // TODO: send info to Rails -- if indefiniteStay is true, then ignore the checkout date since it will be locked out in the app but may have a date selected before the user locked it.
 
 
-    debugger
     // send to rails to generate a stay resource
   },
 
-  initialize: function (options) {
-    this.user = options.model;
+
+  lockCheckOutInput: function () {
+    var $checkOutGroup = $('.check-out-group');
+    $checkOutGroup.toggleClass('lock-check-out');
+    this.toggleCheckOutInput();
   },
+
 
   render: function () {
     var content = this.template({
@@ -35,8 +55,16 @@ FosterPals.Views.DatesPicker = Backbone.CompositeView.extend({
 
   },
 
-  selectDateCheckIn: function (event) {
+  toggleCheckOutInput: function () {
+    var $checkOut = $('#check-out');
+    var checkOutInput = $checkOut.data("DateTimePicker");
+    if (!this.checkOutInputToggled) {
 
+      checkOutInput.disable();
+    } else {
+      checkOutInput.enable();
+    }
+    this.checkOutInputToggled = !this.checkOutInputToggled;
   }
 
 });
