2f95f525dc1ec84ae9e64696111780c5c4d60715
chris <chris@chriss-MacBook-Pro.local>
2015-06-16 14:12:04 -0700

    Revise schema

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -1,5 +1,6 @@
 Fabricator :image do
-  owner_id
+  imageable_id
+  imageable_type
   url {"http://png-3.findicons.com/files/icons/367/ifunny/128/dog.png"}
   thumb_url {"http://png-3.findicons.com/files/icons/367/ifunny/128/dog.png"}
 end
