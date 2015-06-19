2612de69fec0a3003529e61b11f9f7b86f0e7914
Chris Tram <chris.tram@gmail.com>
2015-06-18 17:07:34 -0700

    playing around with css transitions

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,10 +1,15 @@
 class Api::SearchController < ApplicationController
   def location_to_geocode
-    debugger
+    search_location = params[:search_location]
+
+    api_key = ENV['GOOGLE_MAPS_API_KEY']
+    gmaps_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address="
+
+    search_location = search_location.split.join('+')
+    complete_url = gmaps_api_url + search_location + '&key=' + api_key
+    uri = URI(complete_url)
+    response = JSON.parse(Net::HTTP.get(uri))
+    render json: response
   end
-
-
-
-
 
 end
