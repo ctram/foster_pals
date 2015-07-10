9736c26389d293e417524b81c670c2e692b010be
chris <chris.tram@gmail.com>
2015-07-09 17:39:37 -0700

    change seed data

--- spec/fabricators/animal_fabricator.rb
+++ spec/fabricators/animal_fabricator.rb
@@ -2,7 +2,18 @@ Fabricator :animal do
   name {Faker::Name.first_name}
   species { rand(0..1) == 1 ? 'Dog' : 'Cat'}
   sex { rand(0..1) == 1 ? 'm' : 'f'}
-  breed {Faker::Internet.password}
+  # breed {Faker::Internet.password}
+  breed do
+    breeds = [
+      'Pekingese',
+      'Labrador',
+      'Poodle',
+      'Bulldog'
+    ]
+    breeds.sample
+  end
+
+
   color {Faker::Commerce.color}
   # org_id
   # fosterer_id
