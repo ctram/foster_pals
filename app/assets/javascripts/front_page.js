d0e4160213f2a44947d81f9165fe44c14a6a6f8f
Chris Tram <chris.tram@gmail.com>
2018-06-08 18:27:04 -0700

    clean up front page

--- app/assets/javascripts/front_page.js
+++ app/assets/javascripts/front_page.js
@@ -1,21 +1,5 @@
-$(function() {
-  // for smooth scroll when clicking on the 'learn more' button
-  // $('a[href*=#]:not([href=#])').click(function () {
-  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-  //     var target = $(this.hash);
-  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-  //     if (target.length) {
-  //       $('html,body').animate({
-  //         scrollTop: target.offset().top
-  //       }, 1000);
-  //       return false;
-  //     }
-  //   }
-  // });
-
+jQuery(function() {
   jQuery.noConflict();
-  // $('#myModal').modal()
-
   jQuery('.carousel').carousel();
 
   jQuery('a.modal-off').on('click', function(event) {
