45a57cf0cb3de320d4fd53f26390ac342cad156e
Chris Tram <chris.tram@gmail.com>
2018-06-11 16:47:20 -0700

    lint clean up

--- app/assets/javascripts/views/users/profile/contact_island/contact_island.js
+++ app/assets/javascripts/views/users/profile/contact_island/contact_island.js
@@ -2,23 +2,7 @@ FosterPals.Views.ContactIsland = Backbone.CompositeView.extend({
   template: JST['users/profile/contact_island/contact_island'],
 
   initialize: function (options) {
-
     viewingFromScheduler = options.viewingFromScheduler;
-
-    // if (this.model.get('id') !== CURRENT_USER_ID) {
-    //   var contactBtnView = new FosterPals.Views.ContactBtn({
-    //     model: this.model
-    //   });
-    //   this.addSubview('.contact-btns', contactBtnView);
-    //
-    //   if (!viewingFromScheduler) {
-    //     var scheduleBtnView = new FosterPals.Views.SchedulerBtn({
-    //       model: this.model
-    //     });
-    //     this.addSubview('.contact-btns', scheduleBtnView);
-    //   }
-    // }
-
     this.listenTo(this.model, 'sync', this.render);
   },
 
