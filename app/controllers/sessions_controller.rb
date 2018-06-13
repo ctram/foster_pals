7c7422dfd1e2b09712b964ff228ff82bd786f32a
Chris Tram <chris.tram@gmail.com>
2018-06-12 22:16:59 -0700

    separate layouts for static pages and app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:destroy]
 
   def new
+    render layout: 'static_pages'
   end
 
   def create
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,10 @@ class StaticPagesController < ApplicationController
   before_action :redirect_to_front_if_not_signed_in, except: [:front]
 
   def front
+    render layout: 'static_pages'
   end
 
   def app
-    @context = 'app'
+    render layout: 'application'
   end
-
 end
