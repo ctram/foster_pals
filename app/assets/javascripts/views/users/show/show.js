79cd72e7de157d4edec02e55402832064f86c0e3
Chris Tram <chris.tram@gmail.com>
2018-06-13 14:44:50 -0700

    add top padding for profile page

--- app/assets/javascripts/views/users/show/show.js
+++ app/assets/javascripts/views/users/show/show.js
@@ -1,25 +1,25 @@
 FosterPals.Views.UserShow = Backbone.CompositeView.extend({
-  initialize: function (options) {
-    this.user = options.model;
-    var profileView = new FosterPals.Views.Profile({
-      model: this.user
-    });
-    var contactIslandView = new FosterPals.Views.ContactIsland({
-      model: this.user
-    });
-    var aboutView = new FosterPals.Views.About({
-      model: this.user
-    });
-
-    this.addSubview('.profile-hook', profileView);
-    this.addSubview('.about-hook', aboutView);
-    this.listenTo(this.user, 'sync', this.render);
-  },
 
   template: JST['users/show/show'],
 
   className: 'user-show-view',
 
+  initialize: function (options) {
+    this.user = options.model;
+    var profileView = new FosterPals.Views.Profile({
+      model: this.user
+    });  
+    var contactIslandView = new FosterPals.Views.ContactIsland({
+      model: this.user
+    });  
+    var aboutView = new FosterPals.Views.About({
+      model: this.user
+    });  
+
+    this.addSubview('.profile-hook', profileView);
+    this.addSubview('.about-hook', aboutView);
+    this.listenTo(this.user, 'sync', this.render);
+  },  
 
   render: function () {
     var content = this.template({});
@@ -27,7 +27,5 @@ FosterPals.Views.UserShow = Backbone.CompositeView.extend({
     this.attachSubviews();
     return this;
   },
-
-
 
 });
