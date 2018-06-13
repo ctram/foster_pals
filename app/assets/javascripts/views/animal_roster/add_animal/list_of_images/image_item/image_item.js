9968e89d0aa8ed02ee637ee13fb25a6a88113a23
Chris Tram <chris.tram@gmail.com>
2018-06-12 18:42:26 -0700

    lint

--- app/assets/javascripts/views/animal_roster/add_animal/list_of_images/image_item/image_item.js
+++ app/assets/javascripts/views/animal_roster/add_animal/list_of_images/image_item/image_item.js
@@ -1,24 +1,18 @@
 FosterPals.Views.ImageItem = Backbone.CompositeView.extend({
-
   template: JST['animal_roster/add_animal/list_of_images/image_item/image_item'],
 
   className: 'li.image-item',
 
-  initialize: function (options) {
+  initialize: function(options) {},
 
-  },
+  events: {},
 
-  events: {
-  },
-
-  render: function () {
+  render: function() {
     var content = this.template({
       image: this.model
     });
     this.$el.html(content);
     this.attachSubviews();
-
     return this;
-  },
-
+  }
 });
