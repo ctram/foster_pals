2f0e1125e77a26159b91b25938caab5118fcda08
Chris Tram <chris.tram@gmail.com>
2018-06-15 15:23:38 -0700

    adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  },
+  globals: {
+    window: true,
+    FosterPals: true,
+      Backbone: true,
+    CURRENT_USER_ID: true
+  }
+};
