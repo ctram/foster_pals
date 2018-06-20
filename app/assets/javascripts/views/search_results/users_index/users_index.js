9ff0171cb4d9c02b7c39fbc92db15a3504375782
Chris Tram <chris.tram@gmail.com>
2018-06-20 14:25:21 -0700

    linting.

--- app/assets/javascripts/views/search_results/users_index/users_index.js
+++ app/assets/javascripts/views/search_results/users_index/users_index.js
@@ -1,44 +1,40 @@
 FosterPals.Views.UsersIndex = Backbone.CompositeView.extend({
-  initialize: function () {
-    this.listenTo(this.collection, 'add remove sync', this.render);
-    $(window).on('resize', this.setUsersHeight.bind(this));
-  },
-
   className: 'user-items-wrapper',
 
   template: JST['search_results/users_index/users_index'],
 
+  initialize: function() {
+    this.listenTo(this.collection, 'add remove sync', this.render);
+    $(window).on('resize', this.setUsersHeight.bind(this));
+  },
+  
   remove: function() {
     // Removes the listener for dynamically sizing the users pane.
-    $(window).unbind("resize.app");
+    $(window).unbind('resize.app');
     Backbone.View.prototype.remove.call(this);
   },
 
-  removeProfilePreview: function (userId) {
-  },
-
-  render: function () {
-    var content = this.template({
-      users: this.collection
-    });
-    this.$el.html(content);
-    this.setUsersHeight();
-
-    return this;
-  },
-
-  setUsersHeight: function () {
+  setUsersHeight: function() {
     // Dynamically sized scrollable div.
     var height = this.$el.height() - this.$('.sidebar-info').innerHeight();
     this.$('.users').outerHeight(height);
   },
 
-  showProfilePreview: function (userId) {
+  showProfilePreview: function(userId) {
     var user = FosterPals.Collections.users.getOrFetch(userId);
     var profilePreviewView = new FosterPals.Views.ProfilePreview({
       model: user
     });
     this.profilePreviews.push(profilePreviewView);
     this.addSubview('.profile-preview-hook', profilePreviewView);
+  },
+
+  render: function() {
+    var content = this.template({
+      users: this.collection
+    });
+    this.$el.html(content);
+    this.setUsersHeight();
+    return this;
   }
 });
