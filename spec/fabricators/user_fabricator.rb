bf9f4bd03400a8e3e4c1333b44a50c532d3a4986
Chris Tram <chris.tram@gmail.com>
2018-06-20 14:25:37 -0700

    do no automatically create lat and long for a user by fabricator

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -11,16 +11,4 @@ Fabricator :user do
   state { Faker::Address.state_abbr }
   zip_code { Faker::Address.zip_code }
   about_info { Faker::Lorem.paragraph(10) }
-  lat do
-    us_southern_lat = 32.615831
-    us_northern_lat = 41.306330
-    us_lat_range = us_northern_lat - us_southern_lat
-    us_lat = us_lat_range * rand + us_southern_lat
-  end
-  long do
-    us_western_long = -115.031388
-    us_eastern_long = -82.893386
-    us_long_range = us_eastern_long - us_western_long
-    us_long = us_long_range * rand + us_western_long
-  end
 end
